9ed56a53ac9e9f7fbf972fcf212991c47386a4f7
moosaraza8877 <moosaraza8877@gmail.com>
2020-06-09 17:17:32 +0500

    Ready to Commit

---
+++ Practise/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a;
+            Console.Write("Enter your name: ");
+            a = Console.ReadLine();
+            int i = a.IndexOf(' ');
+            int j = a.IndexOf(' ', i + 1);
+            Console.WriteLine("First name: " + a.Substring(0, i));
+            Console.WriteLine("Second name: " + a.Substring(i + 1, j - i));
+            Console.WriteLine("Third name: " + a.Substring(j));
+        }
+    }
+}
